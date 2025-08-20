f895c14c1da7d8490b6d0c72fc826274404e0a35
Janrupf <business.janrupf@gmail.com>
2025-08-20 13:09:43 +0200

    Use Copilot patch from upstream nixpkgs

--- nix/fixup/com.github.copilot.nix
+++ nix/fixup/com.github.copilot.nix
@@ -2,19 +2,8 @@
 , lib
 , glibc
 , gcc-unwrapped
-, buildFHSEnv
-, writeShellScript
-, runtimeShell
 , ...}:
-let
-  copilotFhsEnvs = buildFHSEnv {
-    name = "copilot-jetbrains-fhs";
-
-    runScript = writeShellScript "copilot-run-fhs" ''
-      exec "$@"
-    '';
-  };
-in pkg: pkg.overrideAttrs {
+pkg: pkg.overrideAttrs {
   buildPhase = ''
     agent='copilot-agent/native/${lib.toLower stdenv.hostPlatform.uname.system}${
       {
@@ -23,17 +12,56 @@ in pkg: pkg.overrideAttrs {
       }
       .${stdenv.hostPlatform.uname.processor} or ""
     }/copilot-language-server'
+    orig_size=$(stat --printf=%s $agent)
 
-    mkdir -p "$out/libexec"
+    find_payload_offset() {
+      grep -aobUam1 -f <(printf '\x1f\x8b\x08\x00') "$agent" | cut -d: -f1
+    }
 
-    agentUnwrapped="$out/libexec/copilot-language-server.unwrapped"
-    mv "$agent" "$agentUnwrapped"
+    # Helper: find the offset of the prelude by searching for function string start
+    find_prelude_offset() {
+      local prelude_string='(function(process, require, console, EXECPATH_FD, PAYLOAD_POSITION, PAYLOAD_SIZE) {'
+      grep -obUa -- "$prelude_string" "$agent" | cut -d: -f1
+    }
 
-    cat <<EOF > "$agent"
-      #!${runtimeShell}
-      exec ${copilotFhsEnvs}/bin/copilot-jetbrains-fhs "$agentUnwrapped" "\$@"
-    EOF
+    before_payload_position="$(find_payload_offset)"
+    before_prelude_position="$(find_prelude_offset)"
 
-    chmod +x "$agent"
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $agent
+    patchelf --set-rpath ${
+      lib.makeLibraryPath [
+        glibc
+        gcc-unwrapped
+      ]
+    } $agent
+    chmod +x $agent
+
+    new_size=$(stat --printf=%s $agent)
+    var_skip=20
+    var_select=22
+    shift_by=$(($new_size-$orig_size))
+
+    function fix_offset {
+      # $1 = name of variable to adjust
+      location=$(grep -obUam1 "$1" $agent | cut -d: -f1)
+      location=$(expr $location + $var_skip)
+      value=$(dd if=$agent iflag=count_bytes,skip_bytes skip=$location \
+      bs=1 count=$var_select status=none)
+      value=$(expr $shift_by + $value)
+      echo -n $value | dd of=$agent bs=1 seek=$location conv=notrunc
+    }
+
+    after_payload_position="$(find_payload_offset)"
+    after_prelude_position="$(find_prelude_offset)"
+
+    if [ "${stdenv.hostPlatform.system}" == "aarch64-linux" ]
+    then
+      fix_offset PAYLOAD_POSITION
+      fix_offset PRELUDE_POSITION
+    else
+      # There are hardcoded positions in the binary, then it replaces the placeholders by himself
+      sed -i -e "s/$before_payload_position/$after_payload_position/g" "$agent"
+      sed -i -e "s/$before_prelude_position/$after_prelude_position/g" "$agent"
+    fi
   '';
 }
