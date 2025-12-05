5e6db1035f9f629cd65481d0705e5189e470f2b7
Janrupf <business.janrupf@gmail.com>
2025-12-05 04:40:28 +0100

    Clean up nix packaging for plugins

--- nix/lib/packaging.nix
+++ nix/lib/packaging.nix
@@ -6,60 +6,22 @@
 
 , ... }:
 rec {
-  # Derived from https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/fetchzip/default.nix
-  unpackPlugin = file: fileName: runCommandLocal "extract-intellij-plugin" {
-    input = file;
-    nativeBuildInputs = [ pkgs.unzip glibcLocalesUtf8 ];
-  } ''
-    unpackDir="$TMPDIR/unpack"
-    mkdir "$unpackDir"
-
-    renamedInput="$TMPDIR/${lib.strings.escapeShellArg fileName}"
-    cp "$input" "$renamedInput"
-
-    cd "$unpackDir"
-
-    unpackFile "$renamedInput"
-    chmod -R +w "$unpackDir"
-
-    fn=$(cd "$unpackDir" && ls -A)
-    if [ -f "$unpackDir/$fn" ]; then
-      mkdir $out
-    fi
-    mv "$unpackDir/$fn" "$out"
-
-    chmod 755 "$out"
-  '';
-
-  maybeUnpackPlugin = doUnpack: file: fileName: if doUnpack then unpackPlugin file fileName else file;
-
   createSinglePluginPackage = data: selectedVersion: updateId: fixup:
   let
     updateData = data.updates.${builtins.toString updateId};
     fileName = updateData.file_name or "${data.name}-${selectedVersion}.jar";
   in fixup (pkgs.callPackage ({
-    name ? "jetbrains-plugin-${data.xml_id}",
     version ? selectedVersion,
+    name ? "jetbrains-plugin-${data.xml_id}-${version}",
     sha256 ? updateData.sha256,
     downloadUrl ? updateData.download_url,
     unpack ? lib.strings.hasSuffix ".zip" fileName,
-    fetchAsExecutable ? lib.strings.hasSuffix ".jar" fileName,
     pluginStdenv ? pkgs.stdenvNoCC,
     since ? updateData.since,
     until ? updateData.until,
     compatibilityOverrides ? updateData.compatibility_overrides,
     channel ? updateData.channel,
-  }: pluginStdenv.mkDerivation {
-    name = name;
-    version = version;
-
-    # Download the plugin file
-    src = maybeUnpackPlugin unpack (pkgs.fetchurl {
-      url = downloadUrl;
-      executable = fetchAsExecutable;
-      inherit sha256;
-    }) fileName;
-
+  }: let
     passthru = {
       rawData = data;
       inherit channel;
@@ -70,12 +32,40 @@ rec {
       };
     };
 
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out && cp -r . $out
-      runHook postInstall
-    '';
-  }) {});
+    rawPluginFile = pkgs.fetchurl {
+      url = downloadUrl;
+      name = fileName;
+      inherit sha256 passthru;
+    };
+  in if !unpack
+    # If we are not unpacking either way, no need to build a proxy
+    # derivation - just return the fetched file.
+    #
+    # If anyone wants to fixup the derivation later, they can just
+    # override with `unpack = true` and then apply their fixups.
+    then rawPluginFile
+    else pluginStdenv.mkDerivation {
+      name = name;
+      version = version;
+
+      src = rawPluginFile;
+
+      dontUnpack = !unpack;
+
+      nativeBuildInputs = [
+        # For unpacking
+        pkgs.unzip
+        pkgs.glibcLocalesUtf8
+      ];
+
+      inherit passthru;
+
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out && cp -r . $out
+        runHook postInstall
+      '';
+    }) {});
 
   createAllPluginPackages = data: fixup: let
     channels = lib.attrsets.mapAttrs (_: channelData:
